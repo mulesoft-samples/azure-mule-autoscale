7dfa232c98dd764fb36710229ea900c6569e616e
Rajan Manickavasagam <rajan.manickavasagam@gmail.com>
2019-02-15 20:03:58 +0530

    testing script

--- custom-extension/deploy_mule_api.sh
+++ custom-extension/deploy_mule_api.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# This script will run in the /home/[username] folder in each Linux VM in the VM Scale Set.
 
 sudo apt-get update -y
 sudo apt install -y openjdk-8-jdk
 sudo apt install unzip
-echo `pwd` >> /tmp/text.txt
