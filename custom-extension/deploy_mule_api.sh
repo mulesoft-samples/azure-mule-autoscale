7444d85296df636d838266b2bcfb9ffe6aa45a8c
Rajan Manickavasagam <rajan.manickavasagam@gmail.com>
2019-02-15 19:49:47 +0530

    testing script

--- custom-extension/deploy_mule_api.sh
+++ custom-extension/deploy_mule_api.sh
@@ -4,13 +4,4 @@
 sudo apt-get update -y
 sudo apt install -y openjdk-8-jdk
 sudo apt install unzip
-cd $HOME
-mkdir mule
-cd mule
-wget https://repository-master.mulesoft.org/nexus/content/repositories/releases/org/mule/distributi$
-tar -xvf mule-standalone-3.9.0.tar.gz
-cd mule-standalone-3.9.0/bin
-sh mule start
-cd ../../
-wget -O azure-product-api-39.zip https://github.com/mulesoft-samples/azure-mule-autoscale/blob/mast$
-cp azure-product-api-39.zip mule-standalone-3.9.0/apps
+echo `pwd` >> /tmp/text.txt
