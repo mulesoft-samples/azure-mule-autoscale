02afcdc884949be18ed2f6b74e656d7f6a405b8a
Rajan Manickavasagam <rajan.manickavasagam@gmail.com>
2019-02-15 19:26:25 +0530

    fixed the script issues

--- custom-extension/deploy_mule_api.sh
+++ custom-extension/deploy_mule_api.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # This script will run in the /home/[username] folder in each Linux VM in the VM Scale Set.
 
-sudo adduser --disabled-password --gecos "" mule
-cd /home/mule
-sudo apt-get update -y 
+sudo apt-get update -y
 sudo apt install -y openjdk-8-jdk
 sudo apt install unzip
+cd $HOME
 mkdir mule
 cd mule
-wget https://repository-master.mulesoft.org/nexus/content/repositories/releases/org/mule/distributions/mule-standalone/3.9.0/mule-standalone-3.9.0.tar.gz
+wget https://repository-master.mulesoft.org/nexus/content/repositories/releases/org/mule/distributi$
 tar -xvf mule-standalone-3.9.0.tar.gz
 cd mule-standalone-3.9.0/bin
 sh mule start
 cd ../../
-wget -O azure-product-api-39.zip https://github.com/mulesoft-samples/azure-mule-autoscale/blob/master/product-api/azure-product-api-39.zip?raw=true 
+wget -O azure-product-api-39.zip https://github.com/mulesoft-samples/azure-mule-autoscale/blob/mast$
 cp azure-product-api-39.zip mule-standalone-3.9.0/apps
