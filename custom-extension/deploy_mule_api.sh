7c9d9c576b7c24041bc9906fce6fc98525a32da4
Rajan Manickavasagam <rajan.manickavasagam@gmail.com>
2019-02-16 00:33:05 +0530

    added latest tag to docker image

--- custom-extension/deploy_mule_api.sh
+++ custom-extension/deploy_mule_api.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update -y
 sudo apt install -y docker.io
-sudo docker pull rajanm/muleruntime
+sudo docker pull rajanm/muleruntime:latest
 sudo docker run -d --name mule39instance -p 8081:8081 -v ~/muleapps:/opt/mule/apps -v ~/mulelogs:/opt/mule/logs rajanm/muleruntime
