2602af14e434be02ff89245b5813273e3e5ff34d
Rajan Manickavasagam <rajan.manickavasagam@gmail.com>
2019-02-15 23:44:31 +0530

    added api to container

--- custom-extension/deploy_mule_api.sh
+++ custom-extension/deploy_mule_api.sh
@@ -4,5 +4,3 @@ sudo apt-get update -y
 sudo apt install -y docker.io
 sudo docker pull rajanm/muleruntime
 sudo docker run -d --name mule39instance -p 8081:8081 -v ~/muleapps:/opt/mule/apps -v ~/mulelogs:/opt/mule/logs rajanm/muleruntime
-wget -O azure-product-api-39.zip https://github.com/mulesoft-samples/azure-mule-autoscale/blob/master/product-api/azure-product-api-39.zip?raw=true 
-cp azure-product-api-39.zip ~/muleapps
